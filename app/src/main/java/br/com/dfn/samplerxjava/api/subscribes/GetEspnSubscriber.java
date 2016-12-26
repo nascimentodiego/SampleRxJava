52095c9e1c8793d1b725835746420aba7a9c113f
Diego Nascimento <dfn@cesar.org.br>
2016-12-26 19:25:55 -0300

    refactory EspnObservable

--- app/src/main/java/br/com/dfn/samplerxjava/api/subscribes/GetEspnSubscriber.java
+++
@@ -1,8 +0,0 @@
-package br.com.dfn.samplerxjava.api.subscribes;
-
-import br.com.dfn.samplerxjava.model.News;
-
-public class GetEspnSubscriber extends GenericSubscriber<News> {
-
-
-}
--- app/src/main/java/br/com/dfn/samplerxjava/view/MainActivityFragment.java
+++ app/src/main/java/br/com/dfn/samplerxjava/view/MainActivityFragment.java
@@ -1,37 +1,27 @@
 package br.com.dfn.samplerxjava.view;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 
 import br.com.dfn.samplerxjava.R;
-import br.com.dfn.samplerxjava.api.ApiRequest;
-import br.com.dfn.samplerxjava.api.ServiceClient;
 import br.com.dfn.samplerxjava.api.observables.EspnObservable;
-import br.com.dfn.samplerxjava.app.App;
 import br.com.dfn.samplerxjava.model.Article;
 import br.com.dfn.samplerxjava.model.News;
-import rx.Observable;
 import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     private static final String TAG = "DIEGO";
 
     private TextView txtResult;
-    Subscriber<News> mySubscriber;
+    private Subscriber<News> mySubscriber;
 
     public MainActivityFragment() {
     }
@@ -39,22 +29,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root =  inflater.inflate(R.layout.fragment_main, container, false);
-        txtResult = (TextView)root.findViewById(R.id.txtResult);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        txtResult = (TextView) root.findViewById(R.id.txtResult);
+
         try {
-            callRxJava2();
+            if (savedInstanceState == null) {
+                registerSubscriber();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
         return root;
     }
 
-    public void callRxJava2() throws IOException {
-       EspnObservable espnObservable = new EspnObservable();
-
-
+    public void registerSubscriber() throws IOException {
+        EspnObservable espnObservable = new EspnObservable();
         mySubscriber = new Subscriber<News>() {
             @Override
             public void onCompleted() {
@@ -63,16 +53,16 @@ public class MainActivityFragment extends Fragment {
 
             @Override
             public void onError(Throwable e) {
-                Log.d(TAG,"Subscriber::onError "+ e);
+                Log.d(TAG, "Subscriber::onError " + e);
             }
 
             @Override
             public void onNext(News news) {
-                for (Article art:news.articles) {
-                    Log.d(TAG,"title-art: "+ art.title);
+                for (Article art : news.articles) {
+                    Log.d(TAG, "title-art: " + art.title);
                 }
-                txtResult.setText("RESULT: "+ news.status);
-                Log.d(TAG,"RESULT: "+ news.status);
+                txtResult.setText("RESULT: " + news.status);
+                Log.d(TAG, "RESULT: " + news.status);
             }
 
         };
@@ -84,7 +74,7 @@ public class MainActivityFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
 
-        if(mySubscriber != null && mySubscriber.isUnsubscribed()) {
+        if (mySubscriber != null && mySubscriber.isUnsubscribed()) {
             mySubscriber.unsubscribe();
         }
     }
